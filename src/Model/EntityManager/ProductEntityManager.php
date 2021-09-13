947a74fdb5668c6ab7ff0a8d25fac5d334161a9d
Philipp Hermes <philipphermes2001@gmail.com>
2021-09-13 12:50:05 +0200

    Change ProductEntityManager

--- src/Model/EntityManager/ProductEntityManager.php
+++ src/Model/EntityManager/ProductEntityManager.php
@@ -4,80 +4,43 @@ declare(strict_types=1);
 namespace App\Model\EntityManager;
 
 use App\Model\Database;
+use App\Model\Dto\ProductDataTransferObject;
 use App\Model\Repository\CategoryRepository;
 use App\Model\Repository\ProductRepository;
 
-class ProductEntityManager implements EntityManagerInterface
+class ProductEntityManager
 {
     private \PDO $connection;
     private ProductRepository $productRepository;
-    private CategoryRepository $categoryRepository;
 
-    public function __construct(Database $database, ProductRepository $productRepository, CategoryRepository $categoryRepository)
+    public function __construct(Database $database, ProductRepository $productRepository)
     {
         $this->connection = $database->getConnection();
         $this->productRepository = $productRepository;
-        $this->categoryRepository = $categoryRepository;
     }
 
-    public function insert(array $data = []): ?string
+    public function insert(ProductDataTransferObject $productDTO): void
     {
-        $returnMessage = null;
-        if (isset($data['productname'], $data['categoryID']) && $data['productname'] !== '' && $data['categoryID'] !== '') {
-            if ($this->productRepository->hasProduct(['productname' => $data['productname']]) === false && $this->categoryRepository->hasCategory(['id' => $data['categoryID']]) === true) {
-                $description = $data['description'] ?? null;
+        $queryProduct = $this->connection->prepare('INSERT INTO Product (ProductName, ProductDescription) VALUES (?, ?)');
+        $queryProduct->execute([$productDTO->productname, $productDTO->description]);
 
-                $queryProduct = $this->connection->prepare('INSERT INTO Product (ProductName, ProductDescription) VALUES (?, ?)');
-                $queryCategoryProduct = $this->connection->prepare('INSERT INTO CategoryProduct (CategoryID, ProductID) VALUES (?, ?)');
+        $newProductId = $this->productRepository->getByName($productDTO->productname);
 
-                $queryProduct->execute([$data['productname'], $description]);
-
-                $newProduct = $this->productRepository->getNewID();
-
-                $queryCategoryProduct->execute([$data['categoryID'], $newProduct]); //TODO FIX THIS
-
-                $this->productRepository->map();
-            } else {
-                $returnMessage = "Product already exists or/and Category does not exist";
-            }
-        } else {
-            $returnMessage = "Productname and Category ID musst be given";
-        }
-        return $returnMessage;
+        $queryCategoryProduct = $this->connection->prepare('INSERT INTO CategoryProduct (CategoryID, ProductID) VALUES (?, ?)');
+        $queryCategoryProduct->execute([$productDTO->categoryID, $newProductId]);
     }
 
-    public function update(array $data = []): ?string
+    public function update(ProductDataTransferObject $productDTO): void
     {
-        $returnMessage = null;
-        if (isset($data['productname'], $data['id']) && $data['productname'] !== '' && $data['id'] !== '') {
-            if ($this->productRepository->hasProduct(['id' => $data['id']]) === true) {
-                $description = $data['description'] ?? null;
-
-                $query = $this->connection->prepare('UPDATE Product SET ProductName = ?, ProductDescription = ? WHERE ProductID = ?');
-                $query->execute([$data['productname'], $description, $data['id']]);
-
-                $this->productRepository->map();
-            } else {
-                $returnMessage = "Product doesnt exist";
-            }
-        } else {
-            $returnMessage = "Productname and ID musst be given";
-        }
-        return $returnMessage;
+        $query = $this->connection->prepare('UPDATE Product SET ProductName = ?, ProductDescription = ? WHERE ProductID = ?');
+        $query->execute([$productDTO->productname, $productDTO->description, $productDTO->id]);
     }
 
-    public function delete(array $data  = []): ?string
+    public function delete(int $id): void
     {
-        $returnMessage = null;
-        if(isset($data['id']) && $data['id'] !== ''){
-            $queryProduct = $this->connection->prepare('DELETE FROM Product WHERE ProductID = ? LIMIT 1');
-            $queryCategoryProduct = $this->connection->prepare('DELETE FROM CategoryProduct WHERE ProductID = ? LIMIT 1');
-            $queryCategoryProduct->execute([$data['id']]);
-            $queryProduct->execute([$data['id']]);
-            $this->productRepository->map();
-        } else {
-            $returnMessage = "Id musst be given";
-        }
-        return $returnMessage;
+        $queryProduct = $this->connection->prepare('DELETE FROM Product WHERE ProductID = ? LIMIT 1');
+        $queryProduct->execute([$id]);
+        $queryCategoryProduct = $this->connection->prepare('DELETE FROM CategoryProduct WHERE ProductID = ? LIMIT 1');
+        $queryCategoryProduct->execute([$id]);
     }
 }

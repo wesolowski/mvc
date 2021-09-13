6fde55cfcf93839de0d36f4a36857c810135a0d3
Philipp Hermes <philipphermes2001@gmail.com>
2021-09-13 12:56:14 +0200

    Change UserEntityManager

--- src/Model/EntityManager/UserEntityManager.php
+++ src/Model/EntityManager/UserEntityManager.php
@@ -4,63 +4,32 @@ declare(strict_types=1);
 namespace App\Model\EntityManager;
 
 use App\Model\Database;
-use App\Model\Repository\UserRepository;
+use App\Model\Dto\UserDataTransferObject;
 
-class UserEntityManager implements EntityManagerInterface
+class UserEntityManager
 {
     private \PDO $connection;
-    private UserRepository $userRepository;
 
-    public function __construct(Database $database, UserRepository $userRepository)
+    public function __construct(Database $database)
     {
         $this->connection = $database->getConnection();
-        $this->userRepository = $userRepository;
     }
 
-    public function insert(array $data = []): ?string
+    public function insert(UserDataTransferObject $userDTO): void
     {
-        $returnMessage = null;
-        //Checks if User & Passwort set
-        if (isset($data['username'], $data['password']) && $data['username'] !== '' && $data['password'] !== '') {
-            //Checks if User exists
-            if ($this->userRepository->getByUsername($data['username']) === null) {
-                $query = $this->connection->prepare('INSERT INTO User (Username, Password) VALUES (?, ?)');
-                $query->execute([$data['username'], $data['password']]);
-            } else {
-                $returnMessage = "User already exists";
-            }
-        } else {
-            $returnMessage = "User and Password musst be given";
-        }
-        return $returnMessage;
+        $query = $this->connection->prepare('INSERT INTO User (Username, Password) VALUES (?, ?)');
+        $query->execute([$userDTO->username, $userDTO->password]);
     }
 
-    public function update(array $data = []): ?string
+    public function update(UserDataTransferObject $userDTO): void
     {
-        $returnMessage = null;
-        if (isset($data['username'], $data['password'], $data['id']) && $data['username'] !== '' && $data['password'] !== '' && $data['id'] !== '') {
-            if($this->userRepository->getByID($data['id']) !== null) {
-                $query = $this->connection->prepare('UPDATE User SET Username = ?, Password = ? WHERE UserID = ? LIMIT 1');
-                $query->execute([$data['username'], $data['password'], $data['id']]);
-            }else{
-                $returnMessage = "User does not exist";
-            }
-        } else {
-            $returnMessage = "User, Password and ID musst be given";
-        }
-        return $returnMessage;
+        $query = $this->connection->prepare('UPDATE User SET Username = ?, Password = ? WHERE UserID = ? LIMIT 1');
+        $query->execute([$userDTO->username, $userDTO->password, $userDTO->id]);
     }
 
-    public function delete(array $data  = []): ?string
+    public function delete(int $id): void
     {
-        $returnMessage = null;
-        if(isset($data['id']) && $data['id'] !== ''){
-            $query = $this->connection->prepare('DELETE FROM User WHERE UserID = ?');
-            $query->execute([$data['id']]);
-        }
-        else{
-            $returnMessage = "Id musst be given";
-        }
-        return $returnMessage;
+        $query = $this->connection->prepare('DELETE FROM User WHERE UserID = ?');
+        $query->execute([$id]);
     }
 }

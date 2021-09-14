8953c3c7b6c725cc2681377c9031b0adf41db55d
Philipp Hermes <philipphermes2001@gmail.com>
2021-09-14 13:45:29 +0200

    Fix errors in ProductDetail Controller

--- src/Controller/Frontend/ProductDetail.php
+++ src/Controller/Frontend/ProductDetail.php
@@ -6,6 +6,8 @@ namespace App\Controller\Frontend;
 use App\Core\Container;
 use App\Core\Redirect\RedirectInterface;
 use App\Core\View\ViewInterface;
+use App\Model\Dto\CategoryDataTransferObject;
+use App\Model\Repository\CategoryRepository;
 use App\Model\Repository\ProductRepository;
 use App\Controller\ControllerInterface;
 
@@ -13,7 +15,7 @@ final class ProductDetail implements ControllerInterface
 {
     private ViewInterface $viewInterface;
     private ProductRepository $productRepository;
-    private array $category;
+    private CategoryDataTransferObject $category;
 
     public function __construct(Container $container)
     {
@@ -21,25 +23,17 @@ final class ProductDetail implements ControllerInterface
         $this->productRepository = $container->get(ProductRepository::class);
         $redirect = $container->get(RedirectInterface::class);
 
-        if ((isset($_GET['id']) && $_GET['id'] !== '') && preg_match('/^[\d]\$[A-Z][a-z]*$/', $_GET['category']) && !empty($this->productRepository->getList())) {
-            $this->category = explode('$', $_GET['category']);
-        } else {
-            $redirect->redirect('index_old.php?page=p$Product&category=3$Clothing');
-        }
+        $categoryRepository = $container->get(CategoryRepository::class);
 
+        $this->category = $categoryRepository->getByID((int)$_GET['categoryID']);
     }
 
     public function action(): void
     {
-        $category = [
-            'id' => $this->category[0],
-            'categoryname' => $this->category[1],
-        ];
+        $productID = (int)$_GET['productID'];
+        $product = $this->productRepository->getByID($productID);
 
-        $id = (int)$_GET['id'];
-        $product = $this->productRepository->getByID($id);
-
-        $this->viewInterface->addTlpParam('category', $category);
+        $this->viewInterface->addTlpParam('category', $this->category);
         $this->viewInterface->addTlpParam('product', $product);
 
         $this->viewInterface->addTemplate('productDetail.tpl');
--- template/productDetail.tpl
+++ template/productDetail.tpl
@@ -1,7 +1,7 @@
 {include file="base/header.tpl" title="ProductDetail"}
 <h1>Detail</h1>
 <hr/>
-<a href="../index_old.php?page=p$Category&category={$category.id}${$category.categoryname}">Back to {$category.categoryname}</a>
+<a href="../index.php?area=Consumer&page=Product&categoryID={$category->id}">Back to {$category->categoryname}</a>
 <p>{$product->productname}</p>
 <ul>
     <li>ID: {$product->id}</li>

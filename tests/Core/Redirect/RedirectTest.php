313caae98bd830f134c4cbdeb342b8732eb29944
Philipp Hermes <philipphermes2001@gmail.com>
2021-09-20 15:03:57 +0200

    Add Redirect Test

--- tests/Core/Redirect/RedirectTest.php
+++ tests/Core/Redirect/RedirectTest.php
@@ -3,21 +3,26 @@ declare(strict_types=1);
 
 namespace AppTest\Core;
 
-use App\Core\Redirect;
+use App\Core\Redirect\Redirect;
 use PHPUnit\Framework\TestCase;
 
 class RedirectTest extends TestCase
 {
-    /**
-     * @runInSeparateProcess
-     */
-    public function testRedirect()
-    {
-        $redirect = new Redirect\Redirect();
-        $redirect->redirect('index.php');
+    protected Redirect $redirect;
 
-        $this->assertContains(
-            'Location: index.php', xdebug_get_headers()
-        );
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->redirect = $this->getMockBuilder(Redirect::class)
+            ->onlyMethods(['redirect'])
+            ->getMock();
+        $this->redirect
+            ->expects(self::once())
+            ->method('redirect');
+    }
+
+    public function testRedirect(): void
+    {
+        $this->redirect->redirect('index.php');
     }
 }

6c174f1d2fd53e0ea8824a8ac142b7dcba12c4d1
Philipp Hermes <philipphermes2001@gmail.com>
2021-09-29 15:32:09 +0200

    WIP, always

---
+++ src/Model/User.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User
+{
+    /**
+     * @ORM\id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $password;
+}

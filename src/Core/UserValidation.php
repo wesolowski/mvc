0ddc2657ccd32a72f411efd3e0f0e6e431e9b038
Philipp Hermes <philipphermes2001@gmail.com>
2021-09-29 11:41:16 +0200

    things occurred

--- src/Core/UserValidation.php
+++ src/Core/UserValidation.php
@@ -9,31 +9,36 @@ use App\Model\Repository\UserRepository;
 class UserValidation
 {
     private UserRepository $userRepository;
+    private array $errors = [];
+
     public function __construct(UserRepository $userRepository)
     {
         $this->userRepository = $userRepository;
     }
 
+    private function empty(string $type ,string $value): void
+    {
+        if ($value === '') {
+            $this->errors[] =  $type . ' musst be given';
+        }
+    }
+
     public function validation(array $user = []): array
     {
-        $errors = [];
         $name = trim($user['name']) ?? '';
         $password = trim($user['password']) ?? '';
 
-        if ($name === '') {
-            $errors[] = 'Username musst be given';
-        }
-        if ($password === '') {
-            $errors[] = 'Password musst be given';
-        }
-        if (empty($errors)) {
+        $this->empty('Username', $name);
+        $this->empty('Password', $password);
+
+        if (empty($this->errors)) {
             $userDTO = $this->userRepository->getByUsername($name);
             if (($userDTO instanceof UserDataTransferObject) && $name === $userDTO->name && $password === $userDTO->password) {
                 return [];
             } else {
-                $errors[] = "Password and User don't match";
+                $this->errors[] = "Password and User don't match";
             }
         }
-        return $errors;
+        return $this->errors;
     }
 }
